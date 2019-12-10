c53e2427c3faf03119c056e5da137792ada764cf
kaustubhn1 <kaustubh.narkhede@fidelitservices.com>
2019-12-10 13:02:17 +0530

    function of prime Number

--- primeNumber.py
+++ primeNumber.py
@@ -1,15 +1,17 @@
-num = 6
 
-if num>1:
-    for i in range(2,num//2):
-        if num%i==0:
-            print('NOT PRIME')
-            break
+def primeNumber(num):
+    if num>1:
+        for i in range(2,num//2):
+            if num%i==0:
+                print('NOT PRIME')
+                break
+
+
+        else:
+            print('Prime')
 
 
     else:
-        print('Prime')
+        print('Not Prime')
 
-
-else:
-    print('Not Prime')
+primeNumber(19)
