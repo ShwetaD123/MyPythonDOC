2b8d53d70add5a4211f811496c503c13768dd92b
ShwetaD123 <shwetakdongre@gmail.com>
2019-12-17 14:59:34 +0530

    else chaged

--- Hungry.py
+++ Hungry.py
@@ -3,7 +3,8 @@ hungry = input('Are you hungry?')
 if hungry == 'Yes' :
     print('Eat Samosa')
     print('Eat Pizza')
-    print('Eat ddddd')
+    print('Eat Pizza')
 
 else :
+    print ("I don't want anything")
     print('Go to sleep')
