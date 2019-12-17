a575557e39dda9585a36d60f6b49c1bacfafd194
ShwetaD123 <shwetakdongre@gmail.com>
2019-12-17 14:43:29 +0530

    dddd

--- Hungry.py
+++ Hungry.py
@@ -3,7 +3,7 @@ hungry = input('Are you hungry?')
 if hungry == 'Yes' :
     print('Eat Samosa')
     print('Eat Pizza')
-    print('Eat Burger')
+    print('Eat ddddd')
 
 else :
     print('Go to sleep')
