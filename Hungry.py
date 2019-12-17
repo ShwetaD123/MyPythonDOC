bca2cefbeece988ce3afe707e987cabfcd1e9d5d
ShwetaD123 <shwetakdongre@gmail.com>
2019-12-17 15:17:53 +0530

    Noodles

--- Hungry.py
+++ Hungry.py
@@ -3,7 +3,8 @@ hungry = input('Are you hungry?')
 if hungry == 'Yes' :
     print('Eat Samosa')
     print('Eat Pizza')
-    print('Eat DOSA')
+    print('Eat Noodles')
 
 else :
+    print ("I don't want anything")
     print('Go to sleep')
