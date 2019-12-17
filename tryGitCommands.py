a89f2dbc43f8b08123677fbfae54239ac76762e5
ShwetaD123 <shwetakdongre@gmail.com>
2019-12-17 18:52:40 +0530

    Update Yourself

--- tryGitCommands.py
+++ tryGitCommands.py
@@ -2,6 +2,7 @@ age = int(input('What is your age'))
 if age < 18 :
     print('Students category')
     print('Need to Focus on study')
+    print('Update yourself for Developement of India')
 
 else:
     print('Adult Category.....!')
