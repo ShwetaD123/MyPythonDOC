b41a989cc6f38574c0816282db576a173ab60777
ShwetaD123 <shwetakdongre@gmail.com>
2019-12-17 15:53:07 +0530

    File is reverted

--- Hungry.py
+++ Hungry.py
@@ -4,7 +4,7 @@ if hungry == 'Yes' :
     print('Eat Samosa')
     print('Eat Pizza')
     print('Eat Pizza')
-    print('Eat Rice')
+
 else :
     print ("I don't want anything")
     print('Go to sleep')
