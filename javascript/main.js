5146feca312ac166f49bd5fa708877299ad97a0a
ShwetaD123 <shwetakdongre@gmail.com>
2019-12-18 19:26:45 +0530

    javascript

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+
+function func1()
+        {
+
+            document.getElementById("demo").style.fontSize ="40px";
+            document.getElementById("demo").innerHTML="This is the statement that is change the contain of html from external js file";
+            document.getElementById("demo").style.color="green";
+
+        }
