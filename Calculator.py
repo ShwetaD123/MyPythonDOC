f9f8b0587c0515cec34bdb61b259c470a2143b27
ShwetaD123 <shwetakdongre@gmail.com>
2019-12-18 14:35:47 +0530

    asd

--- Calculator.py
+++ Calculator.py
@@ -8,7 +8,26 @@ def function(num,op):
             print(num+num)
             return 'Addition =',num+num
 
+        elif op == '-':
+            print(num - num)
+            return 'subtraction =', num - num
+
+        elif op == '/':
+            print(num / num)
+            return 'division =', num / num
 
 
-
+<<<<<<< HEAD
 print(function(8,'+')
+=======
+
+
+
+        elif op == '*':
+            print(num * num)
+            return 'Multiplication =', num * num
+
+
+
+print(function(8,'*'))
+>>>>>>> calbranch
