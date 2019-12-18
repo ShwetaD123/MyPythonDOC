f43137d7de7116a14020772d2b9601ae6c1cabb5
ShwetaD123 <shwetakdongre@gmail.com>
2019-12-18 14:25:29 +0530

    Subtraction LOgic

--- Calculator.py
+++ Calculator.py
@@ -8,7 +8,19 @@ def function(num,op):
             print(num+num)
             return 'Addition =',num+num
 
+        elif op == '-':
+            print(num - num)
+            return 'subtraction =', num - num
+
+        elif op == '/':
+            print(num / num)
+            return 'division =', num / num
+
+        elif op == '*':
+            print(num * num)
+            return 'Multiplication =', num * num
 
 
 
-print(function(8,'+'))
+
+print(function(8,'*'))
